fea4592655e9db06b91c345fde3882ba0c115a8c
Jan <jan.kressin@googlemail.com>
2024-12-03 21:22:10 +0100

    initial commit for day 3 part 1

---
+++ AoC_Day03_1/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> lines = InputReader.readInput();
+		lines.forEach(line -> System.out.println(line));
+	}
+
+}
