6c17f5d049f3e0fe4058015b81ce8ed2b79ffd24
Jan <jan.kressin@googlemail.com>
2024-12-22 23:23:10 +0100

    after i just read part 2: nope! good night!

--- AoC_Day13_1/src/Main.java
+++ AoC_Day13_1/src/Main.java
@@ -6,7 +6,7 @@ import org.jksoft.utils.inputreader.InputReader;
 public class Main {
 
 	public static void main(String[] args) {
-		List<String> lines = InputReader.readTestInput(2);
+		List<String> lines = InputReader.readInput();
 		// lines.forEach(System.out::println);
 
 		List<ClawMachine> machines = new ArrayList<>();
@@ -18,7 +18,7 @@ public class Main {
 			int x = Integer.parseInt(splitValues[0].split("\\+")[1]);
 			int y = Integer.parseInt(splitValues[1].split("\\+")[1]);
 
-			Button a = new Button('A', 1, new GridVector(x, y));
+			Button a = new Button('A', 3, new GridVector(x, y));
 
 			values = lines.get(i + 1).split(": ")[1];
 			splitValues = values.split(", ");
@@ -26,7 +26,7 @@ public class Main {
 			x = Integer.parseInt(splitValues[0].split("\\+")[1]);
 			y = Integer.parseInt(splitValues[1].split("\\+")[1]);
 
-			Button b = new Button('B', 3, new GridVector(x, y));
+			Button b = new Button('B', 1, new GridVector(x, y));
 
 			values = lines.get(i + 2).split(": ")[1];
 			splitValues = values.split(", ");
@@ -43,71 +43,88 @@ public class Main {
 		}
 
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (int i = 0; i < 100; i++) {
 			sb.append("a");
 		}
 		for (int i = 0; i < 100; i++) {
 			sb.append("b");
 		}
-		
+
 		String baseString = sb.toString();
-		
-		System.out.println(baseString);
-		
-		for (ClawMachine clawMachine : machines) {
-			int targetX = clawMachine.getTarget().getX();
-			int targetY = clawMachine.getTarget().getY();
-			
-			List<List<Integer>> results = new ArrayList<>();
-			
-			findPermutations(targetX,
-					List.of(clawMachine.getButton('a').getMovement().getX(),
-							clawMachine.getButton('b').getMovement().getY()),
-					new ArrayList<>(), results);
-			
-			for (List<Integer> validResult : results) {
-				System.out.println(validResult);
-			}
+
+		List<String> permutations = new ArrayList<>();
+
+		for (int i = 200; i > 0; i--) {
+			findPermutations(i, baseString, permutations);
 		}
+		
+		int result = 0;
+		
+		for (ClawMachine machine : machines) {
+			System.out.println(machine);
+			
+			int targetX = machine.getTarget().getX();
+			int aButtonX = machine.getButton('a').getMovement().getX();
+			int bButtonX = machine.getButton('b').getMovement().getX();
+			
+			int targetY = machine.getTarget().getY();
+			int aButtonY = machine.getButton('a').getMovement().getY();
+			int bButtonY = machine.getButton('b').getMovement().getY();
+			
+			int aButtonCost = machine.getButton('a').getCost();
+			int bButtonCost = machine.getButton('b').getCost();
+			
+			int cheapest = Integer.MAX_VALUE;
+			
+			for(int i=0; i<permutations.size(); i++) {
+				String permString = permutations.get(i);
+				int aCount = countCharacter(permString, 'a');
+				int bCount = permString.length() - aCount;
+				int permXValue = aCount * aButtonX + bCount * bButtonX;
+				if(permXValue == targetX) {
+					int permYValue = aCount * aButtonY + bCount * bButtonY;
+					if(targetY == permYValue) {
+						int permCost = aCount * aButtonCost + bCount * bButtonCost;
+						if(permCost < cheapest) {
+							System.out.println(aCount + " : " + bCount);
+							System.out.println(aButtonCost + " : " + bButtonCost);
+							cheapest = permCost;
+						}
+					}
+				}
+			}
+			
+			if(cheapest == Integer.MAX_VALUE)
+				cheapest = 0;
+			
+			System.out.println(cheapest);
+			
+			result += cheapest;
+			
+		}
+		System.err.println(result);
 	}
 	
-	/**
-	 * Stupid Dynamic Programming-Approach
-	 * 
-	 * @param target
-	 * @param numbers
-	 * @param current
-	 * @param result
-	 * 
-	 * First: define basecase -> when target hits zero, this is a valid path!
-	 * 
-	 * Second: define exitcase -> 	when target goes below zero, this is an invalid path!
-	 * 
-	 * Third: define algorithm ->	iterate over numbers (the 2 possible values to add up)
-	 * 									add current number (from iteration) to the current result-list (current)
-	 * 									call the method again with current number subtractet from the current target value
-	 * 									backtrack for next iteration
-	 * 
-	 * I hate this!
-	 */
-	public static void findPermutations(int target, List<Integer> numbers, List<Integer> current, List<List<Integer>> result) {
-		System.out.println(current);
-		
-		if (target == 0) {
-			result.add(new ArrayList<>(current));
-			return;
+	private static int countCharacter(String str, char c) {
+		int count = 0;
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == c)
+				count++;
+		}
+		return count;
+	}
+
+	private static void findPermutations(int len, String baseString, List<String> permutations) {
+		int pos = 0;
+		while (baseString.length() >= pos + len) {
+			String current = baseString.substring(pos, pos + len);
+			if (!permutations.contains(current)) {
+				permutations.add(current);
+			}
+			pos++;
 		}
 
-		if (target < 0) {
-			return;
-		}
-
-		for (int number : numbers) {
-			current.add(number);
-			findPermutations(target - number, numbers, current, result);
-			current.remove(current.size() - 1);
-		}
 	}
 
 }
